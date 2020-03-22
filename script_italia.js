918e8caa6c245ae76515550a0856f9f8112fe797
ricfrr <riccardo.franceschini@live.i>
2020-03-22 22:46:17 +0100

    new design

--- script_italia.js
+++ script_italia.js
@@ -13,12 +13,12 @@ window.onload = function () {
     log_recovered = []
     list_date = []
 
-    xmlhttp.onreadystatechange = function() {
-    
-    if (this.readyState == 4 && this.status == 200) {
-        var data = JSON.parse(this.responseText);
-        console.log(data)
-        data.forEach(element => {
+    xmlhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            console.log(data)
+            data.forEach(element => {
                 list_cases.push(element.totale_casi)
                 log_cases.push(Math.log(parseInt(element.totale_casi)))
 
@@ -32,113 +32,51 @@ window.onload = function () {
                 log_death.push(Math.log(parseInt(element.deceduti)))
 
                 date = new Date(element.data)
-                month = date.getMonth()+1
-                list_date.push(date.getDate()+"-"+month)
-        });
-        
+                month = date.getMonth() + 1
+                list_date.push(date.getDate() + "-" + month)
+            });
 
-        var ctx_case = document.getElementById('trend').getContext('2d');
-        var chart_case = new Chart(ctx_case ,{type :'line', 
-                                            data: {
-                    labels: list_date,
-                    datasets: [{
+            console.log(list_actual_positive[list_actual_positive.length - 1])
+            var ctx_case = document.getElementById('general_situation').getContext('2d');
+            var chart_case = new Chart(ctx_case, {
+                type: 'doughnut',
+                data: {
+                    labels : ['positivi attuali','guariti','deceduti'],
+                    datasets : [{
                         label: '# casi',
-                        data: list_cases,
-                        backgroundColor: [
-                            'rgba(205, 226, 15, 0.2)',
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label: '# positivi',
-                        data: list_actual_positive,
-                        backgroundColor: [
-                            'rgba(220,20,60,  0.2)',
-                        ],
-                        borderWidth: 1
-                    },
-                    {
-                        label: '# guariti',
-                        data: list_recovered,
-                        backgroundColor: [
-                            'rgba(127,255,0,  0.2)',
-                        ],
-                        borderWidth: 1
-                    }, 
-                    {
-                        label: '# deceduti',
-                        data: list_death,
-                        backgroundColor: [
-                            'rgba(0,0,0, 0.2)',
-                        ],
-                        borderWidth: 1
-                    }
-                
-                ]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
-                    }
+                        data: [list_actual_positive[list_actual_positive.length - 1],list_recovered[list_recovered.length - 1],list_death[list_death.length - 1]],
+                        backgroundColor : ["#e90b3a", "#a0ff03", "#1ad5de"]
+                    }]
                 }
-         })
+            })
+            
+            document.getElementById("data_total_case").innerHTML = list_cases[list_cases.length-1]
+            diff_total_case = list_cases[list_cases.length-1] - list_cases[list_cases.length-2]
+            document.getElementById("data_total_case_change").innerHTML = "(+"+ String(diff_total_case) + ")"
 
-         var ctx_log_case = document.getElementById('trend_log').getContext('2d');
-         var chart_log_case = new Chart(ctx_log_case ,{type :'line', 
-                                             data: {
-                     labels: list_date,
-                     datasets: [{
-                         label: '# log_casi',
-                         data: log_cases,
-                         backgroundColor: [
-                            'rgba(205, 226, 15, 0.2)',
-                        ],
-                         borderWidth: 1
-                     },
-                     {
-                        label: '# log_positivi',
-                        data: log_actual_positive,
-                        backgroundColor: [
-                            'rgba(220,20,60,  0.2)',
-                       ],
-                        borderWidth: 1
-                    }, 
-                    {
-                        label: '# log_guariti',
-                        data: log_recovered,
-                        backgroundColor: [
-                            'rgba(127,255,0,  0.2)',
-                        ],
-                        borderWidth: 1
-                    }, 
-                    {
-                        label: '# log_deceduti',
-                        data: log_death,
-                        backgroundColor: [
-                            'rgba(0,0,0, 0.2)',
-                        ],
-                        borderWidth: 1
-                    }
-                    ]
-                 },
-                 options: {
-                     scales: {
-                         yAxes: [{
-                             ticks: {
-                                 beginAtZero: true
-                             }
-                         }]
-                     }
-                 }
-          })
+            document.getElementById("data_positive").innerHTML = list_actual_positive[list_actual_positive.length-1]
+            diff_total_case = list_actual_positive[list_actual_positive.length-1] - list_actual_positive[list_actual_positive.length-2]
+            pos = "+"
+            if (diff_total_case < 0){
+                pos = "-"
+            }
+            document.getElementById("data_positive_change").innerHTML = "("+pos+ String(diff_total_case) + ")"
 
-    }};
+            document.getElementById("data_recovered").innerHTML = list_recovered[list_recovered.length-1]
+            diff_total_case = list_recovered[list_recovered.length-1] - list_recovered[list_recovered.length-2]
+            document.getElementById("data_recovered_change").innerHTML = "(+"+ String(diff_total_case) + ")"
+
+            document.getElementById("data_death").innerHTML = list_death[list_death.length-1]
+            diff_total_case = list_death[list_death.length-1] - list_death[list_death.length-2]
+            document.getElementById("data_death_change").innerHTML = "(+"+ String(diff_total_case) + ")"
+            
+
+            
+        }
+    };
     xmlhttp.open("GET", "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json", true);
     xmlhttp.send();
 
 }
+
 
